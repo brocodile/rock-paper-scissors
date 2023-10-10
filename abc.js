2222aae25513ad65bdefcf6fdbee58c718c899ec
brocodile <dakzsh24@gmail.com>
2023-10-10 19:00:41 +0530

    Initial Parameters

--- abc.js
+++ abc.js
@@ -1,5 +1,14 @@
 function getComputerChoice(){
-    const arr = ['Rock','Paper','Scissors'];
-    const random = Math.floor(Math.random() * arr.length);
-    return arr[random];
+    const arr = ['rock','paper','scissors'];
+    const randomness = Math.floor(Math.random() * arr.length);
+    return arr[randomness];
 }
+
+function round(playerSelection,computerSelection){
+
+}
+
+let player = prompt('Rock-Paper-Scissors??');
+player = player.toLowerCase();        // making text lowercase
+let computer = getComputerChoice();
+console.log(round(player,computer));
